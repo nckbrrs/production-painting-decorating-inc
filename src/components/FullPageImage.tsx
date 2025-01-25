169999b33ace0be12fa14e41f177fc9b77dac2a9
nckbrrs <github@nickbarrs.com>
2025-01-25 01:18:04 -0500

    add delete button

--- src/components/FullPageImage.tsx
+++ src/components/FullPageImage.tsx
@@ -1,18 +1,30 @@
 import { Suspense } from "react";
 import ImageFromDB from "./ImageFromDB";
+import { Button } from "./ui/button";
+import { deleteImage } from "~/server/queries";
 
 export default function FullPageImage(props: { id: number }) {
 	return (
-		<div className="flex w-full h-full justify-center items-center">
-			<div className="flex w-1/2 justify-center">
-				<Suspense fallback={
-					<div className="flex flex-col h-full aspect-square justify-center items-center text-white">
-						<h2 className="text-center items-center">Loading...</h2>
-					</div>
-				}>
+		<div className="flex flex-col w-full h-full justify-center items-center">
+			<Suspense fallback={suspenseFallback}>
+				<div className="flex w-1/2 justify-center">
 					<ImageFromDB id={props.id} className="rounded-xl" />
-				</Suspense>
-			</div>
-		</div>
+				</div>
+				<div className="m-4">
+					<form action={async () => {
+						"use server";
+						await deleteImage(props.id)
+					}}>
+						<Button type="submit" variant="destructive">Delete</Button>
+					</form>
+				</div>
+			</Suspense>
+		</div >
 	);
 }
+
+const suspenseFallback = (
+	<div className="flex flex-col h-full aspect-square justify-center items-center text-white">
+		<h2 className="text-center items-center">Loading...</h2>
+	</div>
+)
--- src/server/queries.ts
+++ src/server/queries.ts
@@ -1,5 +1,9 @@
 import "server-only";
 import { db } from "./db";
+import { images } from "./db/schema";
+import { eq } from "drizzle-orm";
+import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
 
 export async function getImages() {
 	const images = await db.query.images.findMany({
@@ -11,10 +15,15 @@ export async function getImages() {
 
 export async function getImage(id: number) {
 	const image = await db.query.images.findFirst({
-		where: (model, { eq }) => eq(model.id, id)
+		where: eq(images.id, id)
 	});
 
 	if (!image) throw new Error("Image not found");
 
 	return image;
+}
+
+export async function deleteImage(id: number) {
+	await db.delete(images).where(eq(images.id, id));
+	redirect("/");
 }
